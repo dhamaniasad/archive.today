3830cdbff8f9d48f7b38630395a9257c2804a3fd
Asad Dhamani <dhamaniasad@gmail.com>
2014-08-20 20:53:01 +0530

    Update archive.today.go
    Fixes issue #1

--- archive.today.go
+++ archive.today.go
@@ -16,9 +16,30 @@ import (
 	"strconv"
 	"strings"
 	"time"
+	"html/template"
 
 	"github.com/docopt/docopt-go"
 )
+var indexHTML = template.Must(template.New("").Parse(`
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>archive.today bundle - {{.Timestamp}}</title>
+</head>
+
+<body>
+
+    <h1>archive.today bundle - {{.Timestamp}}</h1>
+    <ul>
+    {{range .Sites}}
+        <li><a href="{{.Id}}/index.html">{{.Url}}</a></li>
+    {{end}}
+    </ul>
+
+</body>
+</html>
+`))
 
 func commit(target string) string {
 	resp, err := http.PostForm("https://archive.today/submit/",
